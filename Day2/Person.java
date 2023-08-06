aad1b9b897e502818e3c28b98e8b2c7a53c4740d
surbhi Jain <47022009+jainsurbhii@users.noreply.github.com>
2023-08-06 19:26:07 +0530

    Day2

--- Day2/Person.java
+++ Day2/Person.java
@@ -1,16 +1,22 @@
 
   class Person{
-    String name;
-    int age;
+    private String name;
+    private int age;
 
-    Person(String name, int age)
-    {
-      this.name=name;
+    public void setName(String n){
+        name=n;
+    }
+    public  void setAge(int age){
       this.age=age;
     }
+
     public static void main(String[] args) {
-      Person person1=new Person("Alice", 25);
-      Person person2=new Person("Bob", 30);
+      Person person1=new Person();
+      Person person2=new Person();
+      person1.setName("Alice");
+      person1.setAge(25);
+      person2.setName("Bob");
+      person2.setAge(30);
 
       System.out.println("Person 1 Details");
       System.out.println(person1.name);
