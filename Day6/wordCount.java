069f40d6abbdc9d8c2c42a9ed3f596d50347e417
surbhi Jain <47022009+jainsurbhii@users.noreply.github.com>
2023-08-26 00:50:05 +0530

    Day6

---
+++ Day6/wordCount.java
@@ -0,0 +1,37 @@
+import java.io.File; // Import the File class
+import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.Scanner;// Import the Scanner class to read text files
+
+public class Main {
+//function to count words
+    public static int wordCount(String word)  {
+        int count =0;
+        try {
+            File file =new File("input.txt");// filename
+            Scanner scan=new Scanner(file);
+
+            while (scan.hasNextLine()){
+                String fileConcat = scan.nextLine();
+                String temp[] = fileConcat.split("[ ]|[.]|[,]|[ \\t]");
+
+                for (int i = 0; i < temp.length; i++) {
+                    if (word.equalsIgnoreCase(temp[i])) {
+                        count++;
+                    }
+                }
+            }
+
+            scan.close();
+        }
+        //If file not found then msg will be displayed
+        catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+        }
+        return count;
+    }
+    public static void main(String[] args) throws FileNotFoundException {
+        int count=wordCount("JAVA");//function calling
+        System.out.println("Total occurrences of 'java':" + count);
+    }
+}
+
